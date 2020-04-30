697cc870a56906241812b84528cea5599d261e61
josephanusha007 <64574502+josephanusha007@users.noreply.github.com>
2020-04-30 11:02:44 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'forgot',
+    loadChildren: () => import('./forgot/forgot.module').then( m => m.ForgotPageModule)
+  },
+  {
+    path: 'firstform',
+    loadChildren: () => import('./firstform/firstform.module').then( m => m.FirstformPageModule)
+  },
+  {
+    path: 'reviewfirstform',
+    loadChildren: () => import('./reviewfirstform/reviewfirstform.module').then( m => m.ReviewfirstformPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.module').then( m => m.CartPageModule)
+  },
+  
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
